943f10c40e5f42bdaebeb457688b785a2af36a45
Ammar Hasan <ammar.hasan@btinternet.com>
2023-03-10 14:45:23 +0000

    Correction to app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,7 +30,6 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # Honeycomb - OTEL
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -46,6 +45,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+XRayMiddleware(app, xray_recorder)
 
 # Honeycomb - OTEL
 # Initialize automatic instrumentation with Flask
