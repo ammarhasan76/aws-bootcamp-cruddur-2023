db565ace0c184b083aa3e8949c6eaab76595ed88
Ammar Hasan <ammar.hasan@btinternet.com>
2023-03-11 12:52:04 +0000

    X-Ray Subsegment First Attempt

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,12 +1,25 @@
 from datetime import datetime, timedelta, timezone
+# Import the X-Ray SDK
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
+    # X-Ray ---
+    # Start a segment
+    segment = xray_recorder.begin_segment('home_activities')
+
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+
+    dict = {
+      "now": now.isoformat()
+    }
+    
+  segment.put_metadata('key', dict, 'namespace')
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -20,4 +33,15 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+    
+    # X-Ray ---
+    # Start a subsegment
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
+    xray_recorder.end_subsegment()
+    xray_recorder.end_segment()
     return model
